8995f71c60655985e20caa285e20267ff34d4ee6
Zhenwei Shi <zhenweishi88@gmail.com>
2018-03-15 09:41:18 +0100

    Correction of an error of code indent

--- PyrexReader.py
+++ PyrexReader.py
@@ -12,30 +12,30 @@ Img,Mask = PyrexReader.Img_Bimask(img_path,rtstruct_path,ROI)
 
 """
 
-import dicom,os
+import pydicom,os
 import numpy as np
 from skimage import draw
 import SimpleITK as sitk
 import re
+import glob
 
 
-#class DcmRTstruct_Reader:
-def match_ROIid(rtstruct_path,ROI_name):
+# module PyrexReader:
+def match_ROIid(rtstruct_path,ROI_name): # Match ROI id in RTSTURCT to a given ROI name in the parameter file
     mask_vol = Read_RTSTRUCT(rtstruct_path)
     M= mask_vol[0]
     for i in range(len(M.StructureSetROISequence)):
         if str(ROI_name)==M.StructureSetROISequence[i].ROIName:
             ROI_number = M.StructureSetROISequence[i].ROINumber
-#            print ROI_number
+#            print(ROI_number)
             break
     for ROI_id in range(len(M.StructureSetROISequence)):
-        if ROI_number == M.ROIContourSequence[ROI_id].RefdROINumber:
-#            print ROI_number
+        if ROI_number == M.ROIContourSequence[ROI_id].ReferencedROINumber:
+#            print(ROI_number)
             break
     return ROI_id
 
-	
-def ROI_match(ROI,rtstruct_path):
+def ROI_match(ROI,rtstruct_path): # Literal match ROI
     mask_vol=Read_RTSTRUCT(rtstruct_path)
     M=mask_vol[0]
     target = []
@@ -46,39 +46,42 @@ def ROI_match(ROI,rtstruct_path):
         for j in range(0,len(M.StructureSetROISequence)):
             print M.StructureSetROISequence[j].ROIName
             break
-        print 'Input ROI is: '
+        print('Input ROI is: ')
         ROI_name = raw_input()
         target.append(ROI_name)
     print '------------------------------------'
     return target
 
-def Read_scan(path):
-    scan = [dicom.read_file(os.path.join(path, s), force=True) for s in os.listdir(path)]
+def Read_scan(path): # Read scans under the specified path 
+    scan = [pydicom.dcmread(s, force=True) for s in glob.glob(os.path.join(path,'*.dcm'))]
     try:
-        #scan.sort(key = lambda x: int(x.InstanceNumber))
-        scan.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+        scan.sort(key = lambda x: int(x.ImagePositionPatient[2])) # sort slices based on Z coordinate
     except:
-        print "AttributeError: Maybe Dataset does not have attribute 'InstanceNumber'"
+        print('AttributeError: Cannot read scans')
     return scan
 
-def Read_RTSTRUCT(path):
-    scan = [dicom.read_file(os.path.join(path, s), force=True)for s in os.listdir(path)] 
-    return scan
+def Read_RTSTRUCT(path): # Read RTSTRUCT under the specified path
+    try:
+        rt = [pydicom.dcmread(s, force=True) for s in glob.glob(os.path.join(path,'*.dcm'))]
+    except:
+        print('AttributeError: Cannot read RTSTRUCT')
+    return rt
 
-def poly2mask(vertex_row_coords, vertex_col_coords, shape):
+
+def poly2mask(vertex_row_coords, vertex_col_coords, shape): # Mask interpolation
     fill_row_coords, fill_col_coords = draw.polygon(vertex_row_coords, vertex_col_coords, shape)
     mask = np.zeros(shape, dtype=np.bool)
     mask[fill_row_coords, fill_col_coords] = True
     return mask
 
-def get_pixels(scan):
-    image = np.stack([s.pixel_array for s in scan])
-    image = image.astype(np.int16)
+def get_pixels_hu(scans): # Units to Hounsfield Unit (HU) by multiplying rescale slope and add intercept
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16) #convert to int16
     # the code below checks if the image has slope and intercept
     # since MRI images often do not provide these
     try:
-        intercept = scan[0].RescaleIntercept
-        slope = scan[0].RescaleSlope
+        intercept = scans[0].RescaleIntercept
+        slope = scans[0].RescaleSlope
     except AttributeError:
         pass
     else:
@@ -88,49 +91,71 @@ def get_pixels(scan):
         image += np.int16(intercept)    
     return np.array(image, dtype=np.int16)
 
-def Img_Bimask(img_path,rtstruct_path,ROI_name):
-	# Volume of DICOM files and RTstruct files
-    #print 'Loading SCANs and RTSTRUCT ......'
-
+def Img_Bimask(img_path,rtstruct_path,ROI_name): # generating image array and binary mask
+    print('Generating binary mask from RTSTRUCT...')
     img_vol = Read_scan(img_path)
     mask_vol=Read_RTSTRUCT(rtstruct_path)
-	# Slices all have the same basic information including slice size, patient position, etc.
-    IM=img_vol[0]
-    IM_P=get_pixels(img_vol)
+
+    IM=img_vol[0] # Slices usually have the same basic information including slice size, patient position, etc.
+    IM_P=get_pixels_hu(img_vol)
     M=mask_vol[0]
     num_slice=len(img_vol)
-    
-    #print 'Creating binary mask ......'
-
     mask=np.zeros([num_slice, IM.Rows, IM.Columns],dtype=np.uint8)
-    
     xres=np.array(IM.PixelSpacing[0])
     yres=np.array(IM.PixelSpacing[1])
     ROI_id = match_ROIid(rtstruct_path,ROI_name)
-
-    
-#Check DICOM file Modality
+    #Check DICOM file Modality
     if IM.Modality == 'CT' or 'PT':
 		for k in range(len(M.ROIContourSequence[ROI_id].ContourSequence)):    
 			Cpostion_rt = M.ROIContourSequence[ROI_id].ContourSequence[k].ContourData[2]
-			
+#			print(Cpostion_rt)
+            
 			for i in range(num_slice):
-				if Cpostion_rt == img_vol[i].ImagePositionPatient[2]:
+				if Cpostion_rt == img_vol[i].ImagePositionPatient[2]: # match the binary mask and the corresponding slice
 					sliceOK = i
 					break
-					
+#			print('Slice:',sliceOK)
 			x=[]
 			y=[]
 			z=[]
-			
 			m=M.ROIContourSequence[ROI_id].ContourSequence[k].ContourData
-                         
+			for i in range(0,len(m),3):
+				x.append(m[i+1])
+				y.append(m[i+0])
+				z.append(m[i+2])
+        
+			x=np.array(x)
+			y=np.array(y)
+			z=np.array(z)
+#			print('point-2')
+			x-= IM.ImagePositionPatient[1]
+			y-= IM.ImagePositionPatient[0]
+			z-= IM.ImagePositionPatient[2]
+			pts = np.zeros([len(x),3])  
+			pts[:,0] = x
+			pts[:,1] = y
+			pts[:,2] = z
+			a=0
+			b=1
+			p1 = xres
+			p2 = yres
+			m=np.zeros([2,2])             
+			m[0,0]=img_vol[sliceOK].ImageOrientationPatient[a]*p1 
+			m[0,1]=img_vol[sliceOK].ImageOrientationPatient[a+3]*p2
+			m[1,0]=img_vol[sliceOK].ImageOrientationPatient[b]*p1
+			m[1,1]=img_vol[sliceOK].ImageOrientationPatient[b+3]*p2
+#			print('point-3') 
+         # Transform points from reference frame to image coordinates
+			m_inv=np.linalg.inv(m)
+			pts = (np.matmul((m_inv),(pts[:,[a,b]]).T)).T
+			mask[sliceOK,:,:] = np.logical_or(mask[sliceOK,:,:],poly2mask(pts[:,0],pts[:,1],[IM_P.shape[1],IM_P.shape[2]]))
+         
     elif IM.Modality == 'MR':
         slice_0 = img_vol[0]
         slice_n = img_vol[-1]
 
-		# the screen coordinates, including the slice number can then be computed 
-		# using the inverse of this matrix
+		 # the screen coordinates, including the slice number can then be computed 
+		  # using the inverse of this matrix
         transform_matrix = np.r_[slice_0.ImageOrientationPatient[3:], 0, slice_0.ImageOrientationPatient[:3], 0, 0, 0, 0, 0, 1, 1, 1, 1].reshape(4, 4).T # yeah that's ugly but I didn't have enough time to make anything nicer
         T_0 = np.array(slice_0.ImagePositionPatient)
         T_n = np.array(slice_n.ImagePositionPatient)
@@ -142,6 +167,7 @@ def Img_Bimask(img_path,rtstruct_path,ROI_name):
         transform_matrix[:, 1] *= pix_s[0]
         
         transform_matrix = np.linalg.inv(transform_matrix)
+#        print('point-1')
         for s in M.ROIContourSequence[ROI_id].ContourSequence:    
 			Cpostion_rt = np.r_[s.ContourData[:3], 1] # the ROI point to get slice number from
 													  # in homogenous coordinates
@@ -157,42 +183,48 @@ def Img_Bimask(img_path,rtstruct_path,ROI_name):
 			y=[]
 			z=[]			                            
 			m=s.ContourData
-            
-    for i in range(0,len(m),3):
-		x.append(m[i+1])
-		y.append(m[i+0])
-		z.append(m[i+2])
-	
-    x=np.array(x)
-    y=np.array(y)
-    z=np.array(z)
-    
-    x-= IM.ImagePositionPatient[1]
-    y-= IM.ImagePositionPatient[0]
-    z-= IM.ImagePositionPatient[2]
-    pts = np.zeros([len(x),3])	
-    pts[:,0] = x
-    pts[:,1] = y
-    pts[:,2] = z
-    a=0
-    b=1
-    p1 = xres
-    p2 = yres
-    m=np.zeros([2,2])             
-    m[0,0]=img_vol[sliceOK].ImageOrientationPatient[a]*p1 
-    m[0,1]=img_vol[sliceOK].ImageOrientationPatient[a+3]*p2
-    m[1,0]=img_vol[sliceOK].ImageOrientationPatient[b]*p1
-    m[1,1]=img_vol[sliceOK].ImageOrientationPatient[b+3]*p2
-     
-	# Transform points from reference frame to image coordinates
-    m_inv=np.linalg.inv(m)
-    pts = (np.matmul((m_inv),(pts[:,[a,b]]).T)).T
-    mask[sliceOK,:,:] = np.logical_or(mask[sliceOK,:,:],poly2mask(pts[:,0],pts[:,1],[IM_P.shape[1],IM_P.shape[2]]))
+			for i in range(0,len(m),3):
+				x.append(m[i+1])
+				y.append(m[i+0])
+				z.append(m[i+2])
+    	
+			x=np.array(x)
+			y=np.array(y)
+			z=np.array(z)
+#			print('point-2')
+			x-= IM.ImagePositionPatient[1]
+			y-= IM.ImagePositionPatient[0]
+			z-= IM.ImagePositionPatient[2]
+			pts = np.zeros([len(x),3])	
+			pts[:,0] = x
+			pts[:,1] = y
+			pts[:,2] = z
+			a=0
+			b=1
+			p1 = xres
+			p2 = yres
+			m=np.zeros([2,2])             
+			m[0,0]=img_vol[sliceOK].ImageOrientationPatient[a]*p1 
+			m[0,1]=img_vol[sliceOK].ImageOrientationPatient[a+3]*p2
+			m[1,0]=img_vol[sliceOK].ImageOrientationPatient[b]*p1
+			m[1,1]=img_vol[sliceOK].ImageOrientationPatient[b+3]*p2
+#			print('point-3') 
+			# Transform points from reference frame to image coordinates
+			m_inv=np.linalg.inv(m)
+			pts = (np.matmul((m_inv),(pts[:,[a,b]]).T)).T
+			mask[sliceOK,:,:] = np.logical_or(mask[sliceOK,:,:],poly2mask(pts[:,0],pts[:,1],[IM_P.shape[1],IM_P.shape[2]]))
+
     IM_P-=np.min(IM_P)
     IM_P=IM_P.astype(np.float32)
     IM_P/=np.max(IM_P)
     IM_P*=255
-    
-    Img=sitk.GetImageFromArray(IM_P.astype(np.float32))
+        
+    Img=sitk.GetImageFromArray(IM_P.astype(np.float32)) # convert image_array to image
     Mask=sitk.GetImageFromArray(mask)
-    return Img, Mask
+    
+#    image_file_name='pyrex_image.nrrd'
+#    mask_file_name='pyrex_mask.nrrd' 
+#        
+#    sitk.WriteImage(Img,image_file_name) # save image and binary mask locally
+#    sitk.WriteImage(Mask,mask_file_name)
+    return Img, Mask      
